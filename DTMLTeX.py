42658c496f1d08ddf04d2b5ec0f5ad336abf3fcb
Christian Theune <ct@flyingcircus.io>
2002-07-31 12:28:04 +0000

    Look into the changelog ...

--- DTMLTeX.py
+++ DTMLTeX.py
@@ -15,7 +15,7 @@
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: DTMLTeX.py,v 1.1.1.1.8.4 2002/07/24 23:30:44 ctheune Exp $
+# $Id: DTMLTeX.py,v 1.1.1.1.8.5 2002/07/31 12:28:04 ctheune Exp $
 
 """DTML TeX objects."""
 
@@ -25,7 +25,16 @@ from OFS import PropertyManager
 from Globals import HTML, HTMLFile, MessageDialog
 from OFS.content_types import guess_content_type
 from OFS.DTMLMethod import DTMLMethod, decapitate
+from AccessControl import ClassSecurityInfo
+from ZODB.PersistentMapping import PersistentMapping
+import os.path
+from string import strip
+
 from urllib import quote
+
+def join_dicts(a, b):
+    a.update(b)
+    return a
 
 # Colors for the error beautifier
 colors = ["#FFFFFF","#FF9944"]
@@ -49,24 +58,30 @@ def add(self, id, title='', file='', REQUEST=None, submit=None):
 
 class DTMLTeX( DTMLMethod, PropertyManager.PropertyManager):
     """DTML TeX objects are DTML-Methods that produce Postscript from TeX"""
+    
     meta_type='DTML TeX'
+
+    __version__ = "0.3"
+    
+    security = ClassSecurityInfo()
     
     index_html=None # Prevent accidental acquisition
+    manage_filterForm = HTMLFile('dtml/texFilters', globals())
 
     manage_options=({'label':'Edit', 'action':'manage_main'},
-                    {'label':'Upload', 'action':'manage_uploadForm'},
-                    {'label':'View', 'action':''},
-                    {'label':'Proxy', 'action':'manage_proxyForm'},
-                    {'label':'Security', 'action':'manage_access'},
-                   ) + PropertyManager.PropertyManager.manage_options
+                    {'label':'View', 'action':''} ) + \
+                    PropertyManager.PropertyManager.manage_options + \
+                   ( {'label':'Filters', 'action':'manage_filterForm'},
+                     {'label':'Proxy', 'action':'manage_proxyForm'},
+                     {'label':'Security', 'action':'manage_access'} )
 
-    _properties = ( { 'id':'latexbinary', 'type':'string', 'mode':'w'},
-                    { 'id':'mimetype', 'type':'string', 'mode':'w'},
+    _properties = ( { 'id':'defaultfilter', 'type':'selection', 'select_variable': 'filterIds', 
+                      'mode':'w'},
                     { 'id':'temppath', 'type':'string', 'mode':'w'} )
 
-    latexbinary = "/usr/bin/pdftex"
-    mimetype = "application/pdf"
     temppath = "/tmp"
+    defaultfilter = "pdf"
+
 
     __ac_permissions__=(
     ('View management screens',
@@ -78,6 +93,32 @@ class DTMLTeX( DTMLMethod, PropertyManager.PropertyManager):
     ('FTP access', ('manage_FTPstat','manage_FTPget','manage_FTPlist')),
     )
 
+    def __init__(self, *nv, **kw):
+        DTMLTeX.inheritedAttribute('__init__')(self, *nv, **kw)
+        self.filters = PersistentMapping( { 'pdf' : PersistentMapping({ 'ct':'application/pdf', 'path':'/usr/bin/pdflatex', 'ext':'pdf'}),
+                                            'ps' : PersistentMapping({'ct':'application/ps', 'path': os.path.join(os.path.split(__file__)[0],'genlatex'), 'ext':'ps'}) } )
+
+    def __setstate__(self, state):
+        # added upgrade feature
+        DTMLTeX.inheritedAttribute('__setstate__')(self, state)
+
+
+        if not hasattr(self, "__version__"):
+                self.__version__ = "0.2"
+
+        if self.__version__ == "0.2":
+            self.__version__ = "0.3"
+            self.filters = PersistentMapping( { 'pdf' : PersistentMapping( 
+                 { 'ct':'application/pdf', 'path':'/usr/bin/pdflatex', 'ext':'pdf'}),
+                   'ps' : PersistentMapping({'ct':'application/ps', 
+                   'path': os.path.join(os.path.split(__file__)[0],'genlatex'), 'ext':'ps'})})
+            self.defaultfilter = 'pdf'
+
+    security.declareProtected('View management screens', 'filterIds')
+    def filterIds(self):
+        """Lists the Ids of all available filters."""
+        return self.filters.keys()
+        
     def __call__(self, client=None, REQUEST={}, RESPONSE=None, **kw):
         """Render the document given a client object, REQUEST mapping,
         Response, and key word arguments."""
@@ -96,20 +137,18 @@ class DTMLTeX( DTMLMethod, PropertyManager.PropertyManager):
             # wants to see the tex code, no converted postscript or pdf.
             return result
         
+        # Determine which latex filter to use
+        used_filter = REQUEST.get('tex_filter', self.defaultfilter)
+        if not used_filter in self.filterIds():
+            used_filter = self.defaultfilter
+        used_filter = self.filters[used_filter]
+
         # construct the content-type
-        if REQUEST.has_key("tex_ct"):
-            if str(REQUEST["tex_ct"]).find("/") == -1:
-                content_type = self.mimetype + "=" + REQUEST["tex_ct"]
-            else:
-                content_type = REQUEST["tex_ct"]
-        else:
-            content_type = self.mimetype
-            
-        REQUEST.RESPONSE.setHeader("content-type", content_type)
+        REQUEST.RESPONSE.setHeader("content-type", used_filter['ct'])
         
         #make the distilled output from TeX
         try:
-            return latex(self.latexbinary, self.temppath, result)
+            return latex(used_filter['path'], used_filter['ext'], self.temppath, result)
         except 'LatexError', (logdata, texfile):
             # The next lines are the Code-o-Beautifier *G
             tf = texfile.split("\n")
@@ -139,21 +178,58 @@ class DTMLTeX( DTMLMethod, PropertyManager.PropertyManager):
 
             texlist = []
             for item in zip(range(1,len(tf)+1),tf):
-                texlist.append("<tr bgcolor=\"%s\"> <td align=\"right\"><a name=\"line%s\"><code>%s</code></a></td>" \
+                texlist.append("<tr bgcolor=\"%s\"> <td align=\"right\">" \
+                                "<a name=\"line%s\"><code>%s</code></a></td>" \
                                "<td><code>%s</code></td></tr>\n" % \
-                                (colors[item[0] in errorlines], item[0], item[0], item[1]))
+                                (colors[item[0] in errorlines], item[0], 
+                                 item[0], item[1]))
 
             texlogtable = "<table>" + "\n".join(texlist) + "</table>"
 
             # create output document with error infos
             errmsg = "<html><title>LaTeX compilation had errors.</title>" + \
-                      "<body><h1>Latex output</h1>You receive this output, because latex wasn't able to compile your .tex file correctly. <hr>" + \
-                     errlogtable +"<hr> <h2> Used latex content </h2>" + texlogtable + "<hr><font size=\"2\">generated by DTMLLatex / pdftex</font></body></html>"
+                      "<body><h1>Latex output</h1>You receive this output, "+\
+                      "because latex wasn't able to compile your .tex file "+\
+                      "correctly. <hr>" + \
+                     errlogtable +"<hr> <h2> Used latex content </h2>" + \
+                     texlogtable + "<hr><font size=\"2\">generated by "+\
+                     "DTMLTeX</font></body></html>"
 
             RESPONSE.setHeader('Content-Type','text/html')
             return errmsg
 
+    security.declareProtected('View management screens', 'getFilters')
+    def getFilters(self, REQUEST=None):
+        """Returns a list of all filters."""
+        list = [ join_dicts(value, { 'mapid':id }) for (id, value) in 
+                                            self.filters.items() ]
+        return list
 
+    security.declareProtected('Edit DTMLTeXs')
+    def updateFilters(self, REQUEST):
+        """Updates the filter list."""
+        for filter in self.filters.keys():
+            if not REQUEST.has_key(filter):
+                continue
+                
+            filter_data = REQUEST.get(filter)
+            if filter_data.mapid  == "":
+                del(self.filters[filter])
+            else:
+                self.filters[filter] = PersistentMapping({ 'ct': filter_data.ct, 'path': 
+                            filter_data.path, 'ext':filter_data.ext })
+                self._p_changed = 1
+
+        new = REQUEST.get('new', None)
+        if new:
+            if strip(new.mapid) != "" and strip(new.ct) != "" and \
+                strip(new.path) != "":
+                self.filters[new.mapid] = PersistentMapping({'ct':new.ct, 'path':new.path, 
+                                            'ext':new.ext })
+                self._p_changed = 1
+
+        return self.manage_filterForm(self, REQUEST,
+                    manage_tabs_message="Filters updated.")
 
 #### This is for running the latex-command
 from os import chdir, execv, fork, waitpid, unlink
@@ -178,13 +254,13 @@ def tmpcmd ( path, args ):
         pass
     return
 
-def latex(binary, temp, data):
+def latex(binary, ext, temp, data):
     try:
     
         tempfile.tempdir = temp
         base = mktemp()
         tex  = base + ".tex"
-        pdf  = base + ".pdf"
+        output  = base + ".%s" % ext
         log  = base + ".log" 
         
         # create temporary tex file
@@ -217,7 +293,7 @@ def latex(binary, temp, data):
                 if line == "! Emergency stop." or line == "No pages of output.":
                     raise 'LatexError', (stdout, data)
         
-        f  = open( pdf, "r" )
+        f  = open(output, "r")
         out= f.read()
         f.close()
     finally:
@@ -260,6 +336,9 @@ OFS.Image.File.create_temp= create_temp
 
 
 # $Log: DTMLTeX.py,v $
+# Revision 1.1.1.1.8.5  2002/07/31 12:28:04  ctheune
+# Look into the changelog ...
+#
 # Revision 1.1.1.1.8.4  2002/07/24 23:30:44  ctheune
 # better error handling of latex log
 #
